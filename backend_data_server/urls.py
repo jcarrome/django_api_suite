9ff4ae9eba27638809d19931f5c6e12cce930163
PaulPer37 <paulisaacpo@hotmail.com>
2025-07-26 12:24:59 -0500

    cambios

--- backend_data_server/urls.py
+++ backend_data_server/urls.py
@@ -18,8 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("homepage.urls")),  # Esto hace que la raíz apunte a homepage
+    path("homepage/", include("homepage.urls")),
+    path('demo/rest/api/', include('demo_rest_api.urls')),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse("¡Bienvenido a la aplicación Django!")
-    return render(request, "homepage/index.html")
+    return render(request, 'homepage/index.html')
