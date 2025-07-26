f849329edc2bfe0f26f507db85a85858d8a82fc1
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2025-07-26 17:53:54 +0000

    pract20

--- demo_rest_api/urls.py
+++ demo_rest_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("index/", views.DemoRestApi.as_view(), name="demo_rest_api_resources" ),
-   path("<str:id>/", views.DemoRestApiItem.as_view(), name="demo_rest_api_item"),
+   path("index/", views.DemoRestApi.as_view(), name="demo_rest_api_resources"),
+   path("<str:item_id>/", views.DemoRestApiItem.as_view(), name="demo_rest_api_item"),
 ]
